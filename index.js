9ead2ede65225ae745234d84b09205750128d31b
Jay Bedeau-Stark <unflawed@Jays-MacBook-Air.local>
2023-04-16 15:45:33 +0100

    (feat): Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+const db = mongoose.
+const ProductSchema = mongoose.Schema({
+  productId: ''
+});
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/path", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+module.exports.handler = serverless(app);
